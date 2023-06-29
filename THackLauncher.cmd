8b8648266f7b3c3a8b0031c70ae46ccb86c7c26a
GioyeTheDev <138115736+GioyeTheDev@users.noreply.github.com>
2023-06-29 12:54:56 +0200

    Add files via upload

---
+++ THackLauncher.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+echo Enable "Telnet Client" and then close the window.
+C:\Windows\System32\OptionalFeatures.exe
+
+echo 1. Make Sure "Telnet Client" Is Enabled (if not, enable it.)
+echo 2. Make Sure You have WIFI Is Enabled
+
+echo now type "Telnet Telehack.com"
+echo all done! you now have hidden funny commands! try: starwars, aquarium.
+
+cmd
+
